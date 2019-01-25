c69a6979306406b4c6517d3d4d1d11919e3324ac
Pierre Hirel <pierre.hirel@univ-lille.fr>
2019-01-25 17:40:25 +0100

    - fixed a bug causing a crash when reading LAMMPS cutom dump files (*.lmc) that contained auxiliary properties.

--- src/input/in_lammps_custom.f90
+++ src/input/in_lammps_custom.f90
@@ -14,7 +14,7 @@ MODULE in_lmp_c
 !*     Université de Lille, Sciences et Technologies                              *
 !*     UMR CNRS 8207, UMET - C6, F-59655 Villeneuve D'Ascq, France                *
 !*     pierre.hirel@univ-lille.fr                                                 *
-!* Last modification: P. Hirel - 26 May 2016                                      *
+!* Last modification: P. Hirel - 25 Jan. 2019                                     *
 !**********************************************************************************
 !* This program is free software: you can redistribute it and/or modify           *
 !* it under the terms of the GNU General Public License as published by           *
@@ -92,6 +92,8 @@ finished = .FALSE.
 reduced(:) = .FALSE.
 H(:,:) = 0.d0
 IF(ALLOCATED(comment)) DEALLOCATE(comment)
+IF(ALLOCATED(AUX)) DEALLOCATE(AUX)
+IF(ALLOCATED(AUXNAMES)) DEALLOCATE(AUXNAMES)
 !
 !
 100 CONTINUE
@@ -216,40 +218,26 @@ DO WHILE(.NOT.finished)
         CALL ATOMSK_MSG(999,(/msg/),(/0.d0/))
       ENDIF
       !If some columns were not detected we are in trouble
-      IF(xcol==0 .OR. ycol==0 .OR. zcol==0 ) THEN
+      IF(xcol==0 .AND. ycol==0 .AND. zcol==0 ) THEN
         GOTO 810
       ENDIF
       !Allocate auxiliary array if auxiliary properties were detected
       sizeaux = 0
-      IF( typecol.NE.0 ) THEN
-        sizeaux = sizeaux+1
-      ENDIF
-      IF( vx.NE.0 .AND. vy.NE.0 .AND. vz.NE.0 ) THEN
-        sizeaux = sizeaux+3
-      ELSE
-        !If one or more V are absent, ignore all
-        vx = 0
-        vy = 0
-        vz = 0
-      ENDIF
-      IF( fx.NE.0 .AND. fy.NE.0 .AND. fz.NE.0 ) THEN
-        sizeaux = sizeaux+4
-      ELSE
-        !If one or more F are absent, ignore all
-        fx = 0
-        fy = 0
-        fz = 0
-      ENDIF
-      IF( q.NE.0 ) THEN
-        sizeaux = sizeaux+1
-      ENDIF
+      IF( typecol>0 ) sizeaux = sizeaux+1
+      IF( vx>0 ) sizeaux = sizeaux+1
+      IF( vy>0 ) sizeaux = sizeaux+1
+      IF( vz>0 ) sizeaux = sizeaux+1
+      IF( fx>0 ) sizeaux = sizeaux+1
+      IF( fy>0 ) sizeaux = sizeaux+1
+      IF( fz>0 ) sizeaux = sizeaux+1
+      IF( q>0 ) sizeaux = sizeaux+1
       !If other properties are present, their names are in namecol(:)
       !=> count them
       DO i=1,Ncol
-        IF( LEN_TRIM(namecol(i)).NE.0 ) sizeaux = sizeaux+1
+        IF( LEN_TRIM(namecol(i))>0 ) sizeaux = sizeaux+1
       ENDDO
       !
-      IF(sizeaux.NE.0) THEN
+      IF(sizeaux>0) THEN
         ALLOCATE( AUXNAMES(sizeaux) )
         ALLOCATE( AUX( SIZE(P,1), sizeaux ) )
         AUX(:,:) = 0.d0
@@ -259,24 +247,36 @@ DO WHILE(.NOT.finished)
           sizeaux = sizeaux+1
           AUXNAMES(sizeaux) = 'type'
         ENDIF
-        IF( vx.NE.0 .AND. vy.NE.0 .AND. vz.NE.0 ) THEN
-          AUXNAMES(sizeaux+1) = 'vx'
-          AUXNAMES(sizeaux+2) = 'vy'
-          AUXNAMES(sizeaux+3) = 'vz'
-          sizeaux = sizeaux+4
-        ENDIF
-        IF( fx.NE.0 .AND. fy.NE.0 .AND. fz.NE.0 ) THEN
-          AUXNAMES(sizeaux+1) = 'fx'
-          AUXNAMES(sizeaux+2) = 'fy'
-          AUXNAMES(sizeaux+3) = 'fz'
-          sizeaux = sizeaux+3
-        ENDIF
-        IF( q.NE.0 ) THEN
-          AUXNAMES(sizeaux+1) = 'q'
+        IF( vx>0 ) THEN
           sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'vx'
+        ENDIF
+        IF( vy>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'vy'
+        ENDIF
+        IF( vz>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'vz'
+        ENDIF
+        IF( fx>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'fx'
+        ENDIF
+        IF( fy>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'fy'
+        ENDIF
+        IF( fz>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'fz'
+        ENDIF
+        IF( q>0 ) THEN
+          sizeaux = sizeaux+1
+          AUXNAMES(sizeaux) = 'q'
         ENDIF
         DO i=1,Ncol
-          IF( LEN_TRIM(namecol(i)).NE.0 ) THEN
+          IF( LEN_TRIM(namecol(i))>0 ) THEN
             sizeaux = sizeaux+1
             AUXNAMES(sizeaux) = namecol(i)
           ENDIF
@@ -290,7 +290,7 @@ DO WHILE(.NOT.finished)
       DO i=1,SIZE(P,1)
         READ(30,'(a1024)',END=840,ERR=840) temp3
         READ(temp3,*,END=840,ERR=840) (acol(j), j=1,Ncol)
-        IF(idcol.NE.0) THEN
+        IF(idcol>0) THEN
           READ(acol(idcol),*,END=840,ERR=840) id
         ELSE
           id = i
@@ -299,9 +299,9 @@ DO WHILE(.NOT.finished)
           nwarn=nwarn+1
           CALL ATOMSK_MSG(2742,(/""/),(/DBLE(id)/))
         ELSE
-          READ(acol(xcol),*,END=840,ERR=840) a
-          READ(acol(ycol),*,END=840,ERR=840) b
-          READ(acol(zcol),*,END=840,ERR=840) c
+          IF(xcol>0) READ(acol(xcol),*,END=840,ERR=840) a
+          IF(ycol>0) READ(acol(ycol),*,END=840,ERR=840) b
+          IF(zcol>0) READ(acol(zcol),*,END=840,ERR=840) c
           !If reduced, convert to Cartesian coordinates
           !Also, the box was shifted so that the lower bounds are at (0,0,0)
           !=> shift atoms to keep their relative positions to the box
@@ -322,7 +322,7 @@ DO WHILE(.NOT.finished)
           ENDIF
           !
           !Read element if present, otherwise type = element
-          IF( elecol.NE.0 ) THEN
+          IF( elecol>0 ) THEN
             !Could be actual element name or a number
             !Try to pass the string, see if it is an element name
             CALL ATOMNUMBER(acol(elecol),testreal)
@@ -336,7 +336,7 @@ DO WHILE(.NOT.finished)
               READ(acol(typecol),*,END=840,ERR=840) testreal
             ENDIF
             !
-          ELSEIF( typecol.NE.0 ) THEN
+          ELSEIF( typecol>0 ) THEN
             !No 'element' column => use atom type as atomic number
             READ(acol(typecol),*,END=840,ERR=840) testreal
             !
@@ -351,39 +351,53 @@ DO WHILE(.NOT.finished)
           !Read auxiliary properties (if any)
           IF(ALLOCATED(AUX)) THEN
             sizeaux = 0
-            IF( typecol.NE.0 ) THEN
-              READ(acol(typecol),*,END=840,ERR=840) a
-              AUX(id,1) = a
+            IF( typecol>0 ) THEN
               sizeaux = 1
+              READ(acol(typecol),*,END=840,ERR=840) a
+              AUX(id,sizeaux) = a
             ENDIF
-            IF( vx.NE.0 .AND. vy.NE.0 .AND. vz.NE.0 ) THEN
+            IF( vx>0 ) THEN
+              sizeaux = sizeaux+1
               READ(acol(vx),*,END=840,ERR=840) a
-              READ(acol(vy),*,END=840,ERR=840) b
-              READ(acol(vz),*,END=840,ERR=840) c
-              AUX(id,sizeaux+1) = a
-              AUX(id,sizeaux+2) = b
-              AUX(id,sizeaux+3) = c
-              sizeaux = sizeaux+3
+              AUX(id,sizeaux) = a
             ENDIF
-            IF( fx.NE.0 .AND. fy.NE.0 .AND. fz.NE.0 ) THEN
+            IF( vy>0 ) THEN
+              sizeaux = sizeaux+1
+              READ(acol(vy),*,END=840,ERR=840) a
+              AUX(id,sizeaux) = a
+            ENDIF
+            IF( vz>0 ) THEN
+              sizeaux = sizeaux+1
+              READ(acol(vz),*,END=840,ERR=840) a
+              AUX(id,sizeaux) = a
+            ENDIF
+            IF( fx>0 ) THEN
+              sizeaux = sizeaux+1
               READ(acol(fx),*,END=840,ERR=840) a
-              READ(acol(fy),*,END=840,ERR=840) b
-              READ(acol(fz),*,END=840,ERR=840) c
               AUX(id,sizeaux+1) = a
-              AUX(id,sizeaux+2) = b
-              AUX(id,sizeaux+3) = c
-              sizeaux = sizeaux+3
             ENDIF
-            IF( q.NE.0 ) THEN
+            IF( fy>0 ) THEN
+              sizeaux = sizeaux+1
+              READ(acol(fy),*,END=840,ERR=840) a
+              AUX(id,sizeaux+1) = a
+            ENDIF
+            IF( fz>0 ) THEN
+              sizeaux = sizeaux+1
+              READ(acol(fz),*,END=840,ERR=840) a
+              AUX(id,sizeaux+1) = a
+            ENDIF
+            IF( q>0 ) THEN
               READ(acol(q),*,END=840,ERR=840) a
               AUX(id,sizeaux+1) = a
               sizeaux = sizeaux+1
             ENDIF
             DO j=1,Ncol
-              IF( LEN_TRIM(namecol(j)).NE.0 ) THEN
+              IF( LEN_TRIM(namecol(j))>0 ) THEN
                 READ(acol(j),*,END=840,ERR=840) a
                 sizeaux = sizeaux+1
-                AUX(id,sizeaux) = a
+                IF( sizeaux <= SIZE(AUX,2) ) THEN
+                  AUX(id,sizeaux) = a
+                ENDIF
               ENDIF
             ENDDO
           ENDIF
