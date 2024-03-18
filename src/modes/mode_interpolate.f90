1270f3c46876f3d4975f95088774a0ea623d7d29
Pierre Hirel <pierre.hirel@univ-lille.fr>
2024-03-18 14:22:56 +0100

    - in mode "--interpolate", auxiliary properties will now be interpolated between initial and final systems.

--- src/modes/mode_interpolate.f90
+++ src/modes/mode_interpolate.f90
@@ -10,7 +10,7 @@ MODULE mode_interpolate
 !*     Université de Lille, Sciences et Technologies                              *
 !*     UMR CNRS 8207, UMET - C6, F-59655 Villeneuve D'Ascq, France                *
 !*     pierre.hirel@univ-lille.fr                                                 *
-!* Last modification: P. Hirel - 04 June 2021                                     *
+!* Last modification: P. Hirel - 18 March 2024                                    *
 !**********************************************************************************
 !* This program is free software: you can redistribute it and/or modify           *
 !* it under the terms of the GNU General Public License as published by           *
@@ -48,15 +48,17 @@ IMPLICIT NONE
 CHARACTER(LEN=*),INTENT(IN):: file1, file2
 CHARACTER(LEN=*),INTENT(IN):: prefix
 CHARACTER(LEN=5),DIMENSION(:),ALLOCATABLE:: outfileformats
-CHARACTER(LEN=4096):: outputfile
+CHARACTER(LEN=4096):: msg, outputfile
 CHARACTER(LEN=128),DIMENSION(:),ALLOCATABLE:: AUXNAMES1, AUXNAMES2 !names of auxiliary properties of ini.&final images
 CHARACTER(LEN=128),DIMENSION(:),ALLOCATABLE:: AUXNAMESimg          !names of auxiliary properties of an image
 CHARACTER(LEN=128),DIMENSION(:),ALLOCATABLE:: comment, comment1, comment2
 CHARACTER(LEN=128),DIMENSION(:),ALLOCATABLE:: options_array !options and their parameters
+LOGICAL:: doaux    !take auxiliary properties into account?
 LOGICAL:: doshells !also take shells into account?
 LOGICAL,DIMENSION(:),ALLOCATABLE:: SELECT  !mask for atom list
 INTEGER:: i, j, k
 INTEGER,INTENT(IN):: Nimages  !number of configurations to interpolate (that excludes initial & final images)
+INTEGER,DIMENSION(:,:),ALLOCATABLE:: AUXid !index of matching aux.prop. in AUX1, AUX2
 REAL(dp),DIMENSION(3,3):: Huc   !Box vectors of unit cell (unknown, set to 0 here)
 REAL(dp),DIMENSION(3,3):: H1, H2   !Base vectors of the supercells of initial, final images
 REAL(dp),DIMENSION(3,3):: Himg     !Base vectors of the supercell
@@ -70,8 +72,12 @@ REAL(dp),DIMENSION(:,:),ALLOCATABLE:: AUXimg     !auxiliary properties of an ima
 !
 !
 !Initialize variables
+doaux = .FALSE.
 doshells = .FALSE.
+IF(ALLOCATED(Pimg)) DEALLOCATE(Pimg)
+IF(ALLOCATED(Simg)) DEALLOCATE(Simg)
 IF(ALLOCATED(SELECT)) DEALLOCATE(SELECT)
+IF(ALLOCATED(AUXid)) DEALLOCATE(AUXid)
 C_tensor(:,:) = 0.d0
 ORIENT(:,:) = 0.d0
 !
@@ -89,22 +95,72 @@ IF( SIZE(P1,1) .NE. SIZE(P2,1) ) THEN
   RETURN
 ENDIF
 !
-!If one configuration has shells but not the other, ignore shells
-IF( ALLOCATED(S1) .OR. ALLOCATED(S2) ) THEN
+!If both configurations have shells, their positions will be interpolated
+IF( ALLOCATED(S1) .AND. ALLOCATED(S2) ) THEN
   IF( SIZE(S1,1) == SIZE(S2,1) ) THEN
     doshells = .TRUE.
-  ELSE
-    doshells = .FALSE.
-    IF(ALLOCATED(S1)) DEALLOCATE(S1)
-    IF(ALLOCATED(S2)) DEALLOCATE(S2)
   ENDIF
 ENDIF
 !
-!Ignore auxiliary properties
-IF(ALLOCATED(AUX1)) DEALLOCATE(AUX1)
-IF(ALLOCATED(AUX2)) DEALLOCATE(AUX2)
-IF(ALLOCATED(AUXNAMES1)) DEALLOCATE(AUXNAMES1)
-IF(ALLOCATED(AUXNAMES2)) DEALLOCATE(AUXNAMES2)
+IF( .NOT. doshells ) THEN
+  IF(ALLOCATED(S1)) DEALLOCATE(S1)
+  IF(ALLOCATED(S2)) DEALLOCATE(S2)
+ENDIF
+!
+!Deal with auxiliary properties
+IF( ALLOCATED(AUXNAMES1) .AND. ALLOCATED(AUXNAMES2) ) THEN
+  !Auxiliary properties are defined in both systems
+  !Construct a table matching identical properties from both systems
+  ALLOCATE( AUXid( MIN(SIZE(AUXNAMES1),SIZE(AUXNAMES2)) , 2 ) )
+  AUXid(:,:) = 0
+  k=0
+  DO i=1,SIZE(AUXNAMES1)
+    DO j=1,SIZE(AUXNAMES2)
+      IF( AUXNAMES1(i) == AUXNAMES2(j) ) THEN
+        k = k+1
+        AUXid(k,1) = i
+        AUXid(k,2) = j
+      ENDIF
+    ENDDO
+  ENDDO
+  ALLOCATE( AUXNAMESimg(k) )
+  AUXNAMESimg(:) = ""
+  k=0
+  DO i=1,SIZE(AUXNAMES1)
+    DO j=1,SIZE(AUXNAMES2)
+      IF( AUXNAMES1(i) == AUXNAMES2(j) ) THEN
+        k = k+1
+        AUXNAMESimg(k) = TRIM(AUXNAMES1(i))
+      ENDIF
+    ENDDO
+  ENDDO
+  IF( k>0 ) THEN
+    !Matching aux.prop. were found, and their names are now in AUXNAMESimg(:)
+    doaux = .TRUE.
+    IF(verbosity>=4) THEN
+      WRITE(msg,*) "Found ", k, " matching properties:"
+      CALL ATOMSK_MSG(999,(/TRIM(msg)/),(/0.d0/))
+      DO j=1,SIZE(AUXNAMESimg)
+        WRITE(msg,*) j, "  ", TRIM(AUXNAMESimg(j))
+        CALL ATOMSK_MSG(999,(/TRIM(msg)/),(/0.d0/))
+      ENDDO
+    ENDIF
+  ELSE
+    !Found zero matching aux.prop. between the two systems: ignore aux.prop.
+    doaux = .FALSE.
+    IF(ALLOCATED(AUXNAMESimg)) DEALLOCATE(AUXNAMESimg)
+  ENDIF
+ENDIF
+!
+IF(.NOT.doaux) THEN
+  !Ignore auxiliary properties
+  IF(ALLOCATED(AUX1)) DEALLOCATE(AUX1)
+  IF(ALLOCATED(AUX2)) DEALLOCATE(AUX2)
+  IF(ALLOCATED(AUXimg)) DEALLOCATE(AUXimg)
+  IF(ALLOCATED(AUXNAMES1)) DEALLOCATE(AUXNAMES1)
+  IF(ALLOCATED(AUXNAMES2)) DEALLOCATE(AUXNAMES2)
+  IF(ALLOCATED(AUXNAMESimg)) DEALLOCATE(AUXNAMESimg)
+ENDIF
 !
 !
 !
@@ -115,7 +171,6 @@ DO i=0,Nimages+1
   !
   IF( i==0 .OR. i==Nimages+1 ) THEN
     !Initial or final configuration
-    IF(ALLOCATED(Pimg)) DEALLOCATE(Pimg)
     ALLOCATE( Pimg( SIZE(P1,1) , 4) )
     Pimg(:,:) = 0.d0
     !Copy positions of relevant configuration
@@ -128,12 +183,32 @@ DO i=0,Nimages+1
     ENDIF
     !Same with positions of shells, if any
     IF( doshells ) THEN
-      IF(ALLOCATED(Simg)) DEALLOCATE(Simg)
       ALLOCATE( Simg(SIZE(P1,1) , 4) )
       IF( i==0 ) THEN
         Simg = S1
       ELSE
         Simg = S2
+      ENDIF
+    ENDIF
+    !Same with aux.prop, if any
+    IF( doaux ) THEN
+      ALLOCATE( AUXimg(SIZE(P1,1) , SIZE(AUXNAMESimg) ) )
+      IF( i==0 ) THEN
+        DO j=1,SIZE(AUXNAMESimg)
+          DO k=1,SIZE(AUXNAMES1)
+            IF( AUXNAMESimg(j)==AUXNAMES1(k) ) THEN
+              AUXimg(:,j) = AUX1(:,k)
+            ENDIF
+          ENDDO
+        ENDDO
+      ELSE
+        DO j=1,SIZE(AUXNAMESimg)
+          DO k=1,SIZE(AUXNAMES2)
+            IF( AUXNAMESimg(j)==AUXNAMES2(k) ) THEN
+              AUXimg(:,j) = AUX2(:,k)
+            ENDIF
+          ENDDO
+        ENDDO
       ENDIF
     ENDIF
     !
@@ -146,7 +221,6 @@ DO i=0,Nimages+1
     ENDDO
     !
     !Compute interpolated positions, save them into Pimg
-    IF(ALLOCATED(Pimg)) DEALLOCATE(Pimg)
     ALLOCATE( Pimg( SIZE(P1,1) , 4) )
     Pimg(:,:) = 0.d0
     DO j=1,SIZE(P1,1)
@@ -156,12 +230,20 @@ DO i=0,Nimages+1
     !
     !Same with positions of shells, if any
     IF( doshells ) THEN
-      IF(ALLOCATED(Simg)) DEALLOCATE(Simg)
       ALLOCATE( Simg(SIZE(P1,1) , 4) )
       Simg(:,:) = 0.d0
       DO j=1,SIZE(S1,1)
         Simg(j,1:3) = S1(j,1:3) + (S2(j,1:3)-S1(j,1:3))*DBLE(i)/DBLE(Nimages)
         Simg(j,4) = S1(j,4)
+      ENDDO
+    ENDIF
+    !
+    !Interpolate matching aux.prop., save them into AUXimg
+    IF( doaux ) THEN
+      ALLOCATE( AUXimg(SIZE(P1,1),SIZE(AUXNAMESimg) ) )
+      AUXimg(:,:) = 0.d0
+      DO j=1,SIZE(AUXimg,2)
+        AUXimg(:,j) = AUX1(:,AUXid(j,1)) + (AUX2(:,AUXid(j,2))-AUX1(:,AUXid(j,1)))*DBLE(i)/DBLE(Nimages)
       ENDDO
     ENDIF
     !
@@ -171,18 +253,30 @@ DO i=0,Nimages+1
   CALL OPTIONS_AFF(options_array,Huc,Himg,Pimg,Simg,AUXNAMESimg,AUXimg,ORIENT,SELECT,C_tensor)
   !
   !Write current image to file(s)
-  IF(ALLOCATED(comment)) DEALLOCATE(comment)
   ALLOCATE(comment(1))
   WRITE(comment(1),*) i
   outputfile = TRIM(ADJUSTL(prefix))//"_img"//TRIM(ADJUSTL(comment(1)))
   comment(1) = "Image #"//TRIM(ADJUSTL(comment(1)))
   CALL WRITE_AFF(outputfile,outfileformats,Himg,Pimg,Simg,comment,AUXNAMESimg,AUXimg)
+  !
+  !Free memory
+  IF(ALLOCATED(comment)) DEALLOCATE(comment)
+  IF(ALLOCATED(Pimg)) DEALLOCATE(Pimg)
+  IF(ALLOCATED(Simg)) DEALLOCATE(Simg)
+  IF(ALLOCATED(AUXimg)) DEALLOCATE(AUXimg)
 ENDDO
 !
 !
 !
 1000 CONTINUE
-
+IF(ALLOCATED(P1)) DEALLOCATE(P1)
+IF(ALLOCATED(S1)) DEALLOCATE(S1)
+IF(ALLOCATED(P2)) DEALLOCATE(P2)
+IF(ALLOCATED(S2)) DEALLOCATE(S2)
+IF(ALLOCATED(AUX1)) DEALLOCATE(AUX1)
+IF(ALLOCATED(AUX2)) DEALLOCATE(AUX2)
+IF(ALLOCATED(AUXid)) DEALLOCATE(AUXid)
+IF(ALLOCATED(AUXNAMESimg)) DEALLOCATE(AUXNAMESimg)
 !
 !
 END SUBROUTINE INTERPOLATE_XYZ
